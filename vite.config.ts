b4034936500f75af1fce57682cabf1a329f7f43f
Natália <natpsilva2002@gmail.com>
2025-09-26 11:15:18 -0300

    colocando sitemap.xml

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import ViteSitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    ViteSitemap({
+  hostname: 'https://galpoesconstrucoes.com.br',
+  outDir: 'dist'
+})
+
+  ],
   resolve: {
     alias: {
       '@assets': path.resolve('src/assets') // sem __dirname
